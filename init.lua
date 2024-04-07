31774ee704a0efd202fc8cc0140e62b9870c415f
AndyXT <axtreto@me.com>
2024-04-07 12:28:00 -0400

    need to figure out what cli to use.

--- init.lua
+++ init.lua
@@ -1,25 +1,31 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local previewers = require("telescope.previewers")
+local utils = require("telescope.previewers.utils")
 local config = require("telescope.config").values
+
+local log = require("plenary.log"):new()
+log.level = 'debug'
 
 local M = {}
 
 M.show_docker_images = function (opts)
   pickers.new(opts, {
-    finder = finders.new_table({
-      results = {
-      {name = "yes", value = {1,2,3,45}},
-      {name = "no", value = {1,2,3,45}},
-      {name = "maybe", value = {1,2,3,45}},
-      {name = "perhaps", value = {1,2,3,45}},
-      },
+    finder = finders.new_async_job({
+      command_generator = function ()
+        -- command line utility that returns json
+        return {"echo"}
+      end,
 
       entry_maker = function (entry)
-        return {
-          display = entry.name,
-          ordinal = entry.name,
-        }
+        local parsed = vim.json.decode(entry)
+        if parsed then
+          return {
+            value = parsed,
+            display = parsed.name,
+            ordinal = parsed.name,
+          }
+        end
       end,
     }),
     sorter = config.generic_sorter(opts),
@@ -32,8 +38,16 @@ M.show_docker_images = function (opts)
           0,
           0,
           true,
-          vim.tbl_flatten({"hello", "everyone", vim.split(vim.inspect(entry), '\n')})
+          vim.tbl_flatten({
+            "hello",
+            "everyone",
+            "",
+            "```lua",
+            vim.split(vim.inspect(entry.value), '\n'),
+            "```",
+          })
         )
+        utils.highlighter(self.state.bufnr, "markdown")
       end,
     }),
   }):find()
